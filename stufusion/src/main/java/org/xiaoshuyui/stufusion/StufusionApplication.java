522dee9103c18442042d8d1150529ab16ef5e6a7
Chengxi Gu <528490652@qq.com>
2025-05-21 10:01:33 +0800

    chore: create frontend/backend project

---
+++ stufusion/src/main/java/org/xiaoshuyui/stufusion/StufusionApplication.java
@@ -0,0 +1,13 @@
+package org.xiaoshuyui.stufusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StufusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StufusionApplication.class, args);
+	}
+
+}
---
+++ stufusion/src/test/java/org/xiaoshuyui/stufusion/StufusionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiaoshuyui.stufusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StufusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
