999e46d5194cd4aa24144b5504a31c402b5882b4
Chengxi Gu <528490652@qq.com>
2025-05-21 16:08:09 +0800

    chore: api

--- strufusion/src/main/java/org/xiaoshuyui/strufusion/controller/KBFileController.java
+++ strufusion/src/main/java/org/xiaoshuyui/strufusion/controller/KBFileController.java
@@ -2,6 +2,8 @@ package org.xiaoshuyui.strufusion.controller;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.Executors;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.xiaoshuyui.strufusion.common.Result;
 import org.xiaoshuyui.strufusion.service.KBFileService;
 import org.xiaoshuyui.strufusion.util.DocReader;
+import org.xiaoshuyui.strufusion.util.SseUtil;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @Slf4j
@@ -37,4 +42,27 @@ public class KBFileController {
             return Result.error("上传失败 " + e.getMessage());
         }
     }
+
+    @PostMapping(value = "/{kbId}/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public SseEmitter uploadFiles(
+            @Parameter(description = "文件列表", required = true) @RequestPart("files") MultipartFile[] files,
+            @PathVariable("kbId") Long kbId) {
+        SseEmitter emitter = new SseEmitter(36000000L);
+        Executors.newSingleThreadExecutor().execute(() -> {
+            for (var file : files) {
+                try {
+                    kbFileService.extract(kbId, DocReader.extractText(file.getInputStream()));
+                    SseUtil.sseSend(emitter, file.getOriginalFilename() + "上传成功");
+                } catch (Exception e) {
+                    SseUtil.sseSend(emitter, file.getOriginalFilename() + "上传失败 " + e.getMessage());
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
 }
